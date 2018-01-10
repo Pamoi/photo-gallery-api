b6a80f8d235de83e2d32c24c3453e7eca3cd51be
Matthieu Girod <matthieu.girod.ch@gmail.com>
2018-01-10 22:01:47 +0100

    Removed multi source CORS as it does not work with production caching

--- src/AppBundle/EventListener/CorsResponseListener.php
+++ src/AppBundle/EventListener/CorsResponseListener.php
@@ -7,12 +7,10 @@ use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 class CorsResponseListener
 {
     private $allowOrigin;
-    private $allowedOrigins;
 
     public function __construct($allowOrigin)
     {
         $this->allowOrigin = $allowOrigin;
-        $this->allowedOrigins = array_map(function ($o) { return trim($o); }, explode(",", $this->allowOrigin));
     }
 
     public function onKernelResponse(FilterResponseEvent $event)
@@ -21,19 +19,12 @@ class CorsResponseListener
             return;
         }
 
-        $response = $event->getResponse();
+        if ($this->allowOrigin) {
+            $response = $event->getResponse();
 
-        if ($this->allowOrigin === "*") {
             $response->headers->set('Access-Control-Allow-Origin', $this->allowOrigin);
-        } else {
-            $origin = $event->getRequest()->headers->get("Origin");
-
-            if (in_array($origin, $this->allowedOrigins)) {
-                $response->headers->set('Access-Control-Allow-Origin', $origin);
-            }
+            $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type,X-AUTH-TOKEN,x-auth-token');
         }
-
-        $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type,X-AUTH-TOKEN,x-auth-token');
     }
 }
